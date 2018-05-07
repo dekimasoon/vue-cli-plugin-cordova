6cce5e9212eaa0495e865b9bbcd7112217b78cec
dekimasoon <dekimasoon@gmail.com>
2018-05-07 14:51:05 +0900

    feat: install cordova-plugin-statusbar as default

--- generator/index.js
+++ generator/index.js
@@ -11,6 +11,7 @@ module.exports = (api, options, rootOptions) => {
       'cordova-plugin-device': '^2.0.1',
       'cordova-plugin-ionic-webview': '^1.2.0',
       'cordova-plugin-splashscreen': '^5.0.2',
+      'cordova-plugin-statusbar': '^2.4.2',
       'cordova-plugin-whitelist': '^1.3.3',
       'vuetify': '^1.0.14'
     },
@@ -19,6 +20,7 @@ module.exports = (api, options, rootOptions) => {
         'cordova-plugin-device': {},
         'cordova-plugin-ionic-webview': {},
         'cordova-plugin-splashscreen': {},
+        'cordova-plugin-statusbar': {},
         'cordova-plugin-whitelist': {}
       },
       platforms: [
